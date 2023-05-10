0fe56fff6684a18d8defdf2f4f3875b137612317
Michelle Labrin <labrinmartinez@gmail.com>
2023-05-10 23:16:49 +0200

    Pasos a seguir + GetpokeApi de la Api

--- pokeapi.js
+++ pokeapi.js
@@ -9,4 +9,25 @@ creamos init para llamar a la funcion
 ...
 */
 
+const divContainer = document.querySelector(".container");
+const input = document.querySelector(".search");
+
+
+let arrayPokemon = []; // mi primer array
+
+const getPokeApi = async () => {  // mi función asincrona
+    for (let i = 1; i < 150; i++) {  // veo mas claro que recorra hasta 150 con el for tradicional
+        fetch('https://pokeapi.co/api/v2/pokemon/' + i)
+        .then((response) => response.json()) // lo convierto a json
+        .then((resp) => { // y con la respuesta
+            arrayPokemon.push(resp); // lo meto en mi array con .push
+        if (arrayPokemon.length === 150) { // hasta que llegue a 150
+            goTo(arrayPokemon); // y lo mande a mi array
+        }
+        });
+    }
+};
+
+getPokeApi(); // aqui ire renderizando 
+
 
