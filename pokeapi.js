5b2423a098509ae179839c5acdefcd1df3596505
Michelle Labrin <labrinmartinez@gmail.com>
2023-05-10 21:49:12 +0200

    first commit

---
+++ pokeapi.js
@@ -0,0 +1,9 @@
+/* 
+Hacer variable del id "podekex"
+Crear la funcion fetch para tener la info de la API
+hacemos un bucle for para indicar endpoints
+creamos un map para limpiar la informacion 
+Pintar la lista de elementos en el html 
+creamos init para llamar a la funcion
+
+*/
