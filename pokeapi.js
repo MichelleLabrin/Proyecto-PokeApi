975116a7f8e19b1c5826fb0720d32d471a4fa919
Michelle Labrin <labrinmartinez@gmail.com>
2023-05-11 22:49:57 +0200

    first Map / funcion init / mitad del draw en html

--- pokeapi.js
+++ pokeapi.js
@@ -7,27 +7,61 @@ Pintar la lista de elementos en el html
 creamos init para llamar a la funcion
 
 ...
+
 */
+const divContainer$$ = document.querySelector(".container");
+const listPokedex$$ = document.querySelector("#pokedex");
+const input$$ = document.querySelector(".search");
 
-const divContainer = document.querySelector(".container");
-const input = document.querySelector(".search");
+const arrayPokemon = []; 
 
+// funcion async
+const getPokeApi = async () => {  
+    for (let i = 1; i <= 150; i++) {  
+        const response = await fetch ("https://pokeapi.co/api/v2/pokemon/" + i);
+        const resp = await response.json ();
+        arrayPokemon.push(resp);
+    };
+    return arrayPokemon;
+};
+//getPokeApi().then(() => console.log(arrayPokemon)); // aqui compruebo que me regrese datos
 
-let arrayPokemon = []; // mi primer array
+// Mapeo
+const mapPokeApi = (pokeApiSinMapear) => {
+    return pokeApiSinMapear.map((pokemon) => ({
+    id: pokemon.id,
+    name: pokemon.name,
+    image: pokemon.sprites['front_default'],
+    type: pokemon.types.map((type) => type.type.name).join(`, `),
+    }));
+};
 
-const getPokeApi = async () => {  // mi función asincrona
-    for (let i = 1; i < 150; i++) {  // veo mas claro que recorra hasta 150 con el for tradicional
-        fetch('https://pokeapi.co/api/v2/pokemon/' + i)
-        .then((response) => response.json()) // lo convierto a json
-        .then((resp) => { // y con la respuesta
-            arrayPokemon.push(resp); // lo meto en mi array con .push
-        if (arrayPokemon.length === 150) { // hasta que llegue a 150
-            goTo(arrayPokemon); // y lo mande a mi array
-        }
-        });
+// pinto en el HTML
+const drawPokemons = (pokemons) => {
+    listPokedex$$.innerHTML="";
+
+    for (const pokemon of pokemons){
+        let li$$ = document.createElement("li");
+        li$$.classList.add("card")
+        listPokedex$$.appendChild(li$$);
+
+        let newDiv$$ = document.createElement("div");
+        newDiv$$.innerHTML = pokemon.name;
+        li$$.appendChild(newDiv$$);
+
+        let img$$ = document.createElement("img");
+        img$$.setAttribute("src", pokemon.image);
+        img$$.setAttribute("alt", pokemon.name);
+        li$$.appendChild(img$$);
+        
     }
 };
 
-getPokeApi(); // aqui ire renderizando 
-
-
+// funcion init
+const init = async () => {
+    const pokemon = await getPokeApi();
+    //   console.log(characters);
+    const mappedPokeApi = mapPokeApi(pokemon);
+    drawPokemons(mappedPokeApi);
+};
+init();
